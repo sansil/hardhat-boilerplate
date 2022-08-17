72192993291ada50d1e564716dab0cf85a62488c
sansil <sansil32@gmail.com>
2022-08-17 16:37:27 -0300

    network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require('@nomicfoundation/hardhat-toolbox')
 
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.7',
+  defaultNetwork: 'hardhat',
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
 }
