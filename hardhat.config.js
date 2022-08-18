27f2b543d1e46080a18775f265d7659df8551a04
sansil <sansil32@gmail.com>
2022-08-17 21:23:12 -0300

    add verified

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.7',
@@ -15,4 +15,9 @@ module.exports = {
       chainId: 4,
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,6 @@
 
-const { ethers } = require('hardhat')
+const { ethers, run, network } = require('hardhat')
+require("@nomiclabs/hardhat-etherscan")
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
@@ -7,6 +8,30 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Deployed contract to: ${simpleStorage.address}`)
+  //console.log(network.config)
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    // await 6 blocks to give etherscan time to index the address
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args
+    })
+  }
+  catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+
 }
 
 main().then(() => {
